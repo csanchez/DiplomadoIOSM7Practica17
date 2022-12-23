52ead92696b57b6efa2b101d63240bfc30a118cf
Carlos Sánchez Perales <csanchez@sociales.unam.mx>
2022-12-22 20:48:36 -0600

    first commit

---
+++ app/src/main/java/dgitc/unam/autentificaciontoken/Login.kt
@@ -0,0 +1,4 @@
+package dgitc.unam.autentificaciontoken
+
+data class Login (var username:String,var password:String) {
+}
---
+++ app/src/main/java/dgitc/unam/autentificaciontoken/Usuario.kt
@@ -0,0 +1,4 @@
+package dgitc.unam.autentificaciontoken
+
+data class Usuario(var usuario:String,var clave:String,var rol:String,var token:String) {
+}
